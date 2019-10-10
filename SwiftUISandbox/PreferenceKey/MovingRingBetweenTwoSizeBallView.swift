5bc2c6864ef4da20aaa6f9293e38f1211c2b1787
今井 真尚 <masanao_imai@waku-2.com>
2019-10-10 15:45:43 +0900

    Add comment

--- SwiftUISandbox/PreferenceKey/MovingRingBetweenTwoSizeBallView.swift
+++ SwiftUISandbox/PreferenceKey/MovingRingBetweenTwoSizeBallView.swift
@@ -8,33 +8,20 @@
 
 import SwiftUI
 
-// MARK: - PreferenceKey
-
-struct PreferenceData: Equatable {
-    let idx: Int
-    var rect: CGRect
-}
-
-struct CirclePreferenceKey: PreferenceKey {
-    typealias Value = [PreferenceData]
-
-    static var defaultValue: [PreferenceData] = []
-
-    static func reduce(value: inout [PreferenceData], nextValue: () -> [PreferenceData]) {
-        value.append(contentsOf: nextValue())
-    }
-}
-
 // MARK: - Body
 
 // https://qiita.com/takaf51/items/de86221493f313ebfc3c
 // のコードを元にコンパイルエラーを解消したもの。
 // tapAction → onTapGesture に変更
+// 元記事はこちらと思われる
+// https://swiftui-lab.com/communicating-with-the-view-tree-part-1/
 struct MovingRingBetweenTwoSizeBallView: View {
 
     @State private var activeIdx: Int = 0
     @State private var rects: [CGRect] = Array<CGRect>(repeating: CGRect(), count: 2)
-    @State var isStarted:Bool = false
+    // TODO: 初期表示時&画面回転時に円がアニメーションをさせないようにするためのフラグ
+    // 画面回転することで@Stateが初期化されるのは深堀りしたいところ。
+    @State var isStarted: Bool = false
 
     var body: some View {
 
@@ -66,7 +53,7 @@ struct MovingRingBetweenTwoSizeBallView: View {
                 }
             }
 
-            // 外縁
+            // 円の外縁
             Circle()
                 .stroke(Color.blue, lineWidth: 10)
                 .frame(width: rects[activeIdx].size.width, height: rects[activeIdx].size.height)
@@ -86,6 +73,26 @@ struct BGView: View {
                 .fill(Color.clear)
                 .preference(key: CirclePreferenceKey.self, value: [PreferenceData(idx: self.idx, rect: geometry.frame(in: .named("myCoordination")))])
         }
+    }
+}
+
+// MARK: - PreferenceKey
+
+// PerferenceDataは、Equatableに準拠する必要がある
+struct PreferenceData: Equatable {
+    let idx: Int
+    var rect: CGRect
+}
+
+// 親Viewが参照する値
+struct CirclePreferenceKey: PreferenceKey {
+    // 親Viewに参照させるエイリアス
+    typealias Value = [PreferenceData]
+    // PreferenceKeyに値が設定されていない場合のデフォルト値
+    static var defaultValue: [PreferenceData] = []
+    // 親Viewはこのreduce functionを通じて値を取得する
+    static func reduce(value: inout [PreferenceData], nextValue: () -> [PreferenceData]) {
+        value.append(contentsOf: nextValue())
     }
 }
 
